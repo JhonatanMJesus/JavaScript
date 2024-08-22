e9aad87b68983efc5ddb2503cdd2f80b8bc22b8b
Jhonatan Marcelino <134547236+JhonatanMJesus@users.noreply.github.com>
2024-08-22 12:07:05 -0300

    Add files via upload

---
+++ vetor.js
@@ -0,0 +1,12 @@
+let nomes = [] // Vetor em JavaScript
+
+for(let i=1; i <= 5; i++) {
+    nomes[i] = prompt("Digite o nome da posição " + i + ":");
+}
+
+// Acessando posições de um vetor
+console.log("O nome da posição 1 é: " + nomes[1]);
+console.log("O nome da posição 2 é: " + nomes[2]);
+console.log("O nome da posição 3 é: " + nomes[3]);
+console.log("O nome da posição 4 é: " + nomes[4]);
+console.log("O nome da posição 5 é: " + nomes[5]);
